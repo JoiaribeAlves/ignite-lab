e35f0bfab4e591a8819abc09f935cd7e329d2ecc
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-06-29 01:24:58 -0400

    Aula 2

---
+++ src/components/Video.tsx
@@ -0,0 +1,11 @@
+export function Video() {
+	return (
+		<div className="flex-1">
+			<div className="bg-black flex justify-center">
+				<div className="h-full w-full max-h-[60vh] max-w-[1100px] aspect-video"></div>
+			</div>
+
+			<div></div>
+		</div>
+	);
+}
